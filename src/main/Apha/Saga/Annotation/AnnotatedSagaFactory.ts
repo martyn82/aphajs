34cfe58d7b6cd768724d4368d47f888dd687830b
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-05-07 23:01:13 +0200

    Add AnnotatedSaga factory.

---
+++ src/main/Apha/Saga/Annotation/AnnotatedSagaFactory.ts
@@ -0,0 +1,35 @@
+
+import {SagaFactory} from "../SagaFactory";
+import {AnnotatedSaga} from "./AnnotatedSaga";
+import {ParameterResolver} from "../../Decorators/ParameterResolver";
+import {ClassNameInflector} from "../../Inflection/ClassNameInflector";
+import {SagaType} from "../Saga";
+import {AssociationValues} from "../AssociationValues";
+import {UnsupportedSagaException} from "../UnsupportedSagaException";
+
+export class AnnotatedSagaFactory<T extends AnnotatedSaga | AnnotatedSaga> implements SagaFactory<T> {
+    constructor(private parameterResolver: ParameterResolver) {}
+
+    public createSaga(sagaType: SagaType<T>, id: string, associationValues: AssociationValues): T {
+        if (!this.supports(sagaType)) {
+            let sagaClass = ClassNameInflector.className(sagaType);
+            throw new UnsupportedSagaException(sagaClass);
+        }
+
+        let saga = new sagaType(id, associationValues);
+
+        for (let associationValue of associationValues) {
+            saga.associateWith(associationValue);
+        }
+
+        return saga;
+    }
+
+    public supports(sagaType: SagaType<T>): boolean {
+        return true;
+    }
+
+    public hydrate(saga: T): void {
+        saga.setParameterResolver(this.parameterResolver);
+    }
+}
---
+++ src/test/Apha/Saga/Annotation/AnnotatedSagaFactory.spec.ts
@@ -0,0 +1,66 @@
+
+import * as sinon from "sinon";
+import {expect} from "chai";
+import {AnnotatedSaga} from "../../../../main/Apha/Saga/Annotation/AnnotatedSaga";
+import {AnnotatedSagaFactory} from "../../../../main/Apha/Saga/Annotation/AnnotatedSagaFactory";
+import {UnsupportedSagaException} from "../../../../main/Apha/Saga/UnsupportedSagaException";
+import {AssociationValues} from "../../../../main/Apha/Saga/AssociationValues";
+import {ParameterResolver} from "../../../../main/Apha/Decorators/ParameterResolver";
+import {DefaultParameterResolver} from "../../../../main/Apha/Decorators/DefaultParameterResolver";
+import {AssociationValue} from "../../../../main/Apha/Saga/AssociationValue";
+
+describe("AnnotatedSagaFactory", () => {
+    let factory;
+    let parameterResolver;
+
+    beforeEach(() => {
+        parameterResolver = new DefaultParameterResolver();
+        factory = new AnnotatedSagaFactory<AnnotatedSaga>(parameterResolver);
+    });
+
+    describe("createSaga", () => {
+        it("creates a saga of given type", () => {
+            let saga = factory.createSaga(AnnotatedSagaFactorySpecSaga, "id", new AssociationValues([
+                new AssociationValue("foo", "bar")
+            ]));
+
+            expect(saga instanceof AnnotatedSagaFactorySpecSaga).to.be.true;
+        });
+
+        it("throws exception for unsupported saga type", () => {
+            let factory = new AnnotatedSagaFactory<AnnotatedSagaFactorySpecSaga>(parameterResolver);
+            let factoryMock = sinon.mock(factory);
+
+            factoryMock.expects("supports")
+                .once()
+                .withArgs(AnnotatedSagaFactorySpecSaga2)
+                .returns(false);
+
+            expect(() => {
+                factory.createSaga(AnnotatedSagaFactorySpecSaga2, "id", new AssociationValues());
+            }).to.throw(UnsupportedSagaException);
+        });
+    });
+
+    describe("hydrate", () => {
+        it("hydrates the saga", () => {
+            let saga = new AnnotatedSagaFactorySpecSaga("id");
+            factory.hydrate(saga);
+
+            expect(saga.getId()).to.equal("id");
+            expect(saga.getAssociationValues()).to.eql(new AssociationValues());
+            expect(saga.getParameterResolver()).to.eql(parameterResolver);
+        });
+    });
+});
+
+class AnnotatedSagaFactorySpecSaga extends AnnotatedSaga {
+    public getParameterResolver(): ParameterResolver {
+        return this.parameterResolver;
+    }
+}
+class AnnotatedSagaFactorySpecSaga2 extends AnnotatedSaga {
+    public getParameterResolver(): ParameterResolver {
+        return this.parameterResolver;
+    }
+}
