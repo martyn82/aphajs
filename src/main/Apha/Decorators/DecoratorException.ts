204ab2364da4d932f8eb4baa8250f6018bf99223
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-05-15 14:51:17 +0200

    More generic name parameter.

--- src/main/Apha/Decorators/DecoratorException.ts
+++ src/main/Apha/Decorators/DecoratorException.ts
@@ -2,7 +2,7 @@
 import {Exception} from "../../Exception";
 
 export class DecoratorException extends Exception {
-    constructor(targetClass: string, methodName: string, decoratorName: string) {
-        super(`Invalid '${decoratorName}' decorator for '${targetClass}::${methodName}'.`);
+    constructor(targetClass: string, name: string, decoratorName: string) {
+        super(`Invalid '${decoratorName}' decorator for '${targetClass}::${name}'.`);
     }
 }
