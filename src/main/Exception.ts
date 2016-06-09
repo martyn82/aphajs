4dae39e4a99cc5a6f48d101206b12ada6b396cd1
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-06-09 15:04:53 +0200

    Make exception better.

--- src/main/Exception.ts
+++ src/main/Exception.ts
@@ -1,10 +1,26 @@
 
 import {Inflect} from "./Inflect";
 
-export abstract class Exception implements Error {
+export declare class Error {
+    name: string;
+    message: string;
+    stack: string;
+
+    constructor(message?: string);
+}
+
+export abstract class Exception extends Error {
     public name: string;
+    public stack;
 
     constructor(public message?: string) {
+        super(message);
+
         this.name = Inflect.classOf(this);
+        this.stack = (<any>new Error()).stack;
+    }
+
+    public toString(): string {
+        return this.name + ": " + this.message + "\n" + this.stack;
     }
 }
