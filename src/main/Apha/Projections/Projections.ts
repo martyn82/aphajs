4540a63139e25a6694f5490fa95db5bb9a090857
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-06-09 15:05:31 +0200

    Make projections just be an event listener.

--- src/main/Apha/Projections/Projections.ts
+++ src/main/Apha/Projections/Projections.ts
@@ -1,10 +1,10 @@
 
-import {TypedEventListener} from "../EventHandling/TypedEventListener";
+import {EventListener} from "../EventHandling/EventListener";
 import {ProjectionStorage} from "./Storage/ProjectionStorage";
 import {Projection} from "./Projection";
+import {Event} from "../Message/Event";
 
-export abstract class Projections<T extends Projection> extends TypedEventListener {
-    constructor(protected storage: ProjectionStorage<T>) {
-        super();
-    }
+export abstract class Projections<T extends Projection> implements EventListener {
+    constructor(protected storage: ProjectionStorage<T>) {}
+    public on: (event: Event) => void;
 }
